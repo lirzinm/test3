11f7fb890a9c0e6ff159754f5e121be73f43ad4c
mathi <lirzinm>
2021-12-01 09:43:06 +0100

    MoneyTest nouveau main

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import demo.Money;
-
-public class Main {
-
-    public static void main(String[] args){
-        Money somme = new Money(42,"EUR");
-        System.out.println(somme.toString());
-    }
-}
